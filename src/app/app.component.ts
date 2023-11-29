694db1276af006735aee25478b2b028f0d5998de
wallapsantos <wallanpereira09@gmail.com>
2023-11-29 18:09:56 -0300

    feat: opção de login ou registro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
 import {AuthContentComponent} from "./auth-content/auth-content.component";
 import {HeaderComponent} from "./header/header.component";
+import {ContentComponent} from "./content/content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AuthContentComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, AuthContentComponent, HeaderComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
